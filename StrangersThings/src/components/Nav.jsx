1359b535452a7dff0e2f7c75591174695bd44b9f
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2025-08-02 20:07:31 -0700

    Updated feat

--- StrangersThings/src/components/Nav.jsx
+++ StrangersThings/src/components/Nav.jsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
+import "./Nav.css"; // if you want to keep styling separate
 
-export default function Nav() {
+const Nav = () => {
   return (
-    <nav>
-      <Link to="/posts">See all posts</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-    </nav>
+    <header className="navbar">
+      <h1 className="site-title">Strangers' Things</h1>
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/posts">See All Posts</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/register">Register</Link>
+      </nav>
+    </header>
   );
-}
+};
+
+export default Nav;
+
+// export default function Nav() {
+//   return (
+//     <nav>
+//       <Link to="/posts">See all posts</Link>
+//       <Link to="/login">Login</Link>
+//       <Link to="/register">Register</Link>
+//     </nav>
+//   );
+// }
