12d465e0d780331a3e5a3a8d97c131782e67877c
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2025-08-04 14:19:12 -0700

    chore: remove duplicate Messages import from App.jsx

--- StrangersThings/src/App.jsx
+++ StrangersThings/src/App.jsx
@@ -4,20 +4,24 @@ import Register from "./components/Register";
 import Login from "./components/Login";
 import PostsList from "./components/PostsList";
 import PostCard from "./components/PostCard";
+import Messages from "./components/Messages";
 import Nav from "./components/Nav";
 import "./App.css";
 
 function App() {
   const [token, setToken] = useState(null);
+
   return (
     <>
-      <Nav />
+      <Nav token={token} />
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/register" element={<Register setToken={setToken} />} />
         <Route path="/login" element={<Login setToken={setToken} />} />
         <Route path="/post/:postId" element={<PostCard token={token} />} />
         <Route path="/posts" element={<PostsList token={token} />} />
+        <Route path="/messages" element={<Messages token={token} />} />{" "}
+        {/* ✅ */}
       </Routes>
     </>
   );
